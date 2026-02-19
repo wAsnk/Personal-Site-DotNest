fc219364612f9b6d70e86285b8d98ffa041fb072
Krisztián Németh <krisztian.nemeth@lombiq.com>
2026-02-19 17:03:00 +0100

    Generate HTML from React

---
+++ src/Themes/PersonalSite.Theme/Design/tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./home.html",
+    "./rsvp.html",
+    "./details.html",
+    "./gifts.html",
+    "./story.html",
+    "./gallery.html",
+    "./traditions.html",
+    "./further-details.html",
+    "./styles/tailwind.css",
+  ],
+}
