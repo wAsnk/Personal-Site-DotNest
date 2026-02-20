921a97dc84601d07edaefd54cde03f75abb79441
Krisztián Németh <krisztian.nemeth@lombiq.com>
2026-02-20 15:40:26 +0100

    Implement design

---
+++ src/Themes/PersonalSite.Theme.Starter/Manifest.cs
@@ -0,0 +1,16 @@
+using Lombiq.Hosting.MediaTheme.Bridge.Constants;
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Theme Name",
+    Author = "",
+    Version = "0.0.1",
+    Website = "",
+    Description = "",
+    Dependencies =
+    [
+        "OrchardCore.Themes",
+        FeatureNames.MediaThemeBridge
+    ]
+)]
+
